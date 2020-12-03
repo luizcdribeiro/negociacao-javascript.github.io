d1a8f7285a2ff9cf2c8dfbc3d88f0955e18a434f
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2020-12-03 17:54:30 -0300

    Sistema de inserção de dados numa tabela com Javascript puro

---
+++ js/app/helpers/DateHelper.js
@@ -0,0 +1,27 @@
+class DateHelper {
+
+
+  constructor() {
+    throw new Error('DateHelper não pode ser instanciada')
+  }
+
+  static dataParaTexto(data) {
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+  }
+
+  static textoParaData(texto) {
+
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto)) {
+      throw new Error('Deve estar no formato ano-mes-dia');
+    }
+
+    return new Date(...texto.split('-').map((item, indice) => {
+      if(indice == 1) {
+        return item - 1
+      }
+
+      return item;
+    }))
+  }
+
+}
